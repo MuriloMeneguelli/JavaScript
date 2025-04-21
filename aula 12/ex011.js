180e1ef79a6e5c5fc56c0e582977f17b5733cafc
Murilo Meneguelli <meneguellim7@gmail.com>
2025-04-21 11:10:46 -0300

    .

---
+++ aula 12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 22
+console.log(`Você tem ${idade} anos`)
+if (Idade < 16) {
+    console.log("Não Vota")
+} else if (idade < 18 || idade > 65) {
+    // Voto opcional
+        console.log("Voto Opcional")
+    } else {
+        console.log ("Voto Obrigatório")
+    }
---
+++ aula 12/ex012.js
@@ -0,0 +1,10 @@
+ var hora = 22
+ console.log ('Agora são exatamente ${hora} Horas')
+ if (hora < 12) {
+    console.log('Bom Dia!')
+ } else if (hora <= 18){
+    console.log('Boa Tarde!')
+ } else {
+    console.log ('Boa Noite!')
+ }
+ 
