36747a853df6f2dd28b299e403ac5528c5f2d533
Murilo Meneguelli <meneguellim7@gmail.com>
2025-04-21 13:50:39 -0300

    .

--- ex014/script.js
+++ ex014/script.js
@@ -1,35 +1,45 @@
-function verificar () {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
+function verificar() {
+    var data = new Date();
+    var ano = data.getFullYear();
+    var fano = document.getElementById('txtano');
+    var res = document.getElementById('res'); // Certifique-se de que essa é a div azul!
+
     if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('[ERRO] Verifique os dados e tente novamente!')
-    } 
-    var fsex = document.getElementsByName('radsex')
-    var idade = ano - Number(fano.value)
-    var genero = ''
-    var img = document.createElement('img')
-    img.setAttribute('id', 'foto')
+        window.alert('[ERRO] Verifique os dados e tente novamente!');
+        return;
+    }
+
+    var fsex = document.getElementsByName('radsex');
+    var idade = ano - Number(fano.value);
+    var genero = '';
+    var img = document.createElement('img');
+    img.setAttribute('id', 'foto');
+
     if (fsex[0].checked) {
-        genero = 'Homem'
+        genero = 'Homem';
         if (idade >= 0 && idade < 10) {
-            // Criança
-            img.setAttribute('src', '/homem-crianca.png')
+            img.setAttribute('src', 'imagens/foto-bebe-m.png');
         } else if (idade < 21) {
-            // Jovem
-            img.setAttribute('src', 'bebe-m.png')
+            img.setAttribute('src', 'imagens/foto-jovem-m.png');
         } else if (idade < 50) {
-            // Adulto
-            img.setAttribute('src', 'jovem-m.png')
+            img.setAttribute('src', 'imagens/foto-jovem-m.png');
         } else {
-            // Idoso
-            img.setAttribute('src', 'idoso-m.png')
+            img.setAttribute('src', 'imagens/foto-idoso-m.png');
+        }
+    } else if (fsex[1].checked) {
+        genero = 'Mulher';
+        if (idade >= 0 && idade < 10) {
+            img.setAttribute('src', 'imagens/foto-bebe-f.png');
+        } else if (idade < 21) {
+            img.setAttribute('src', 'imagens/foto-jovem-f.png');
+        } else if (idade < 50) {
+            img.setAttribute('src', 'imagens/foto-jovem-f.png');
+        } else {
+            img.setAttribute('src', 'imagens/foto-idoso-f.png');
         }
     }
-    else if (fsex[1].checked) {
-        genero = 'Mulher'
-    }
-    res.style.textAlign = 'center'
-    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+
+    res.style.textAlign = 'center';
+    res.innerHTML = `Idade calculada: ${idade} anos.`;
+    res.appendChild(img); // Aqui garantimos que a imagem será adicionada dentro da div azul!
 }
